de61d41b69bb84740bc9aa3b029238df513ee18c
calbaby <pedro.caldas009@gmail.com>
2024-04-30 14:11:22 -0300

    oi de marcelo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("hey otarios")
 print('oi de caldas, oi gente ')
 print('oi danilo voce quem voce ama mais Bia fragoso ou Laura Miranda? SKIBIDI???????? AAAHHHHHHHHHHHHHHHHHHHHHH')
+print('oi de marcelo')
