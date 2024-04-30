5947a725e9c9c914d71a39d99d5acaf4b13c65b3
calbaby <pedro.caldas009@gmail.com>
2024-04-30 15:26:19 -0300

    main atualizado e sugerir

--- main.py
+++ main.py
@@ -3,3 +3,28 @@ import adicionar
 import visualizar
 import atualizar
 import excluir
+
+while True:
+
+    print('''MENU
+        0 - Sair
+        1 - Adicionar
+        2 - Visualizar
+        3 - Atualizar
+        4 - Excluir
+        5 - Sugerir
+          ''')
+
+    acao = int(input('Bem-vindo Rafael! Digite o que deseja: '))
+
+    if acao == 0:
+        break
+    elif acao == 1:
+        
+    elif acao == 2:
+
+    elif acao == 3:
+
+    elif acao == 4:
+
+    elif acao == 5:
