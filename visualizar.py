cd36d94ec00c3e2e1cf14f1cca70c0075bb2c06e
Celio Junior <cpdj@cesar.school>
2024-05-06 19:40:00 -0300

    teste não concluido

--- visualizar.py
+++ visualizar.py
@@ -1,11 +1,8 @@
 def visualizar():
-    while True:
-        resposta=input('Deseja ver as receitas: se sim [S] se não [N]: ').upper()
-        if resposta=='s':
+
             with open("./data/receitas.csv", "r") as file:
                 lista=file.readlines()
             for itens in lista:
                 print(f'{itens}\t')
-        else:
-            break
-
+    
+        
