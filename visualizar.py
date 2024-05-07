0f9b8148cac139d80f491677e291c6cf8366723a
Celio Junior <cpdj@cesar.school>
2024-05-07 17:05:56 -0300

    ainda não pronto

--- visualizar.py
+++ visualizar.py
@@ -1,8 +1,9 @@
 def visualizar():
-
-            with open("./data/receitas.csv", "r") as file:
-                lista=file.readlines()
-            for itens in lista:
-                print(f'{itens}\t')
-    
-        
+    resposta=input('você deseja visualizar essa planilha: ')
+    if resposta=='s':
+        file=open("./data/receitas.csv",'r')
+        print(file.read())
+        file.close()
+        resposta=input('pressione qualquer tecla para sair ')
+    else:
+        print('')
