185c2fd5430eb0369cc5d314f4c0e3503b0fc770
João Danilo Correia Jota <jdcj@cesar.school>
2024-05-16 14:28:27 -0300

    excluir parte 1

---
+++ excluir.py
@@ -0,0 +1,7 @@
+def excluir():
+    file = open("./data/receitas.csv", "r")
+
+    apagar = input('Digite o nome da receita que você deseja excluir : ')
+
+    for line in file:
+        print(line)
