9a439bf6c552a565569761465a47e985dcfa5f8d
calbaby <pedro.caldas009@gmail.com>
2024-05-16 17:30:22 -0300

    excluir indentado

--- excluir.py
+++ excluir.py
@@ -1,17 +1,21 @@
 def excluir():
     file = open("./data/receitas.csv", "r")
-    lines=file.readlines()
+    lines = file.readlines()
 
     apagar = input('Digite o nome da receita que você deseja excluir: ')
     index_deletar = -1
-    for index,line in enumerate(lines):
-        receita=line.split(',')
+
+    for index, line in enumerate(lines):
+        receita = line.split(',')
+
         if apagar == receita[0]:
             index_deletar = index
+
     if index_deletar == -1:
         print('Receita não encontrada')
     else:
         lines.pop(index_deletar)
+
     file.close()
 
     file2 = open("./data/receitas.csv", "w")
