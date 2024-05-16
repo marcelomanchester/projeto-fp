13355d81eb3aca848c25ffbe212ee9433a4b4b0e
calbaby <pedro.caldas009@gmail.com>
2024-05-16 15:47:54 -0300

    atualizar em progresso

--- atualizar.py
+++ atualizar.py
@@ -1,11 +1,56 @@
+def atualizacao(receita):
+    while True:
+        atualizacao = input(
+            'O que você deseja atualizar? Nome [N], País [P], Ingredientes [I], Modo de Preparo [M], Favorito [F] ou Finalizar atualização [B]: ').upper()
+
+        if atualizacao == 'N' or atualizacao == 'P' or atualizacao == 'I' or atualizacao == 'M' or atualizacao == 'F' or atualizacao == 'B':
+            if atualizacao == 'B':
+                break
+            elif atualizacao == 'N':
+                novo_nome = input('Substituição: ')
+                receita[0] = novo_nome
+            elif atualizacao == 'P':
+                novo_pais = input('Substituição: ')
+                receita[1] = novo_pais
+            elif atualizacao == 'I':
+                novo_ingrediente = input('Substituição: ')
+                receita[2] = novo_ingrediente
+            elif atualizacao == 'M':
+                novo_preparo = input('Substituição: ')
+                receita[3] = novo_preparo
+            elif atualizacao == 'F':
+                while True:
+                    novo_favorito = input('Favoritos? [S] ou [N]: ').upper()
+
+                    if novo_favorito == 'S' or novo_favorito == 'N':
+                        if novo_favorito == 'S':
+                            receita[4] = True
+                            break
+                        else:
+                            receita[4] = False
+                            break
+                    else:
+                        print('Caractere inválido')
+        else:
+            print('Caractere inválido')
+        return f'{receita[0]}, {receita[1]}, {';'.join(receita[2])}, {';'.join(receita[3])}, {receita[4]}\n'
+
+
 def atualizar():
-    receitas = open("./data/receitas.csv", "r")
+    file = open("./data/receitas.csv", "r")
+    receitas = file.readlines()
 
     nome = input('Digite a receita que você deseja atualizar: ')
 
-    for receita in receitas:
-        if receita['Nome'] == nome:
-            atualizacao = input(
-                'O que você deseja atualizar? Nome [N], País [P], Ingredientes [I], Modo de Preparo [M], Favorito [F]: ')
+    for index, receita in receitas:
+        receita_array = receita.split(',')
 
-    receitas.close()
+        if receita_array[0] == nome:
+            receita_atualizada = atualizacao(receita_array)
+            receitas[index] = receita_atualizada
+            break
+
+    file.close()
+
+    file = open("./data/receitas.csv", "w")
+    file.writelines(f'{receita},')
--- excluir.py
+++ excluir.py
@@ -1,7 +1,7 @@
 def excluir():
     file = open("./data/receitas.csv", "r")
 
-    apagar = input('Digite o nome da receita que você deseja excluir : ')
+    apagar = input('Digite o nome da receita que você deseja excluir: ')
 
     for line in file:
         print(line)
